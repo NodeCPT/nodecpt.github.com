6089e6b44e55311390d826f157c761ad1fae831c
Adrian Rossouw <adrian@developmentseed.org>
2012-04-30 05:47:42 +0200

    tweak the twitter/github colors and result count.

--- _includes/js/twitter.js
+++ _includes/js/twitter.js
@@ -57,7 +57,7 @@ views.Tweets = Backbone.View.extend({
     },
     render: function(options) {
         var self = this;
-        this.collection.chain().first(9).each(function (m) {
+        this.collection.chain().first(12).each(function (m) {
             self.$el.append(self.template({model: m}));
         });
         $('.tweet').popover({placement: 'top'});
